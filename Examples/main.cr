8de08402b6425fb57661f02fd1cda77a970fd3db
ArtificialVoid1 <160069526+ArtificialVoid1@users.noreply.github.com>
2024-04-03 13:24:14 -0400

    Update main.cr

--- Examples/main.cr
+++ Examples/main.cr
@@ -1,1 +1,15 @@
+#define variables
+#var <name> = <value>,<mod>
+var greet = "hello!"
+var name = "my name is shane"
+var math = "1 + 1 = 2!"
+var questionpt1 = "what do "
+var questionpt2 = "you like?"
 
+#print statements
+#print(<value>)
+print(greet)
+print(name)
+print(math)
+print(questionpt1)
+print(questionpt2)
